464e18badb6f7834b1599868ddf16df866fbe739
Kuni Sai Sumanth <saisumanthkuni@gmail.com>
2021-05-24 21:18:13 +0530

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Sai Sumanth on 24/05/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
